c21ce0a1bb578388a931fd2bbcc14355ba6a49f1
Johnny <figurativelobotomy@gmail.com>
2020-11-26 00:32:17 -0800

    Finalized JS programming

--- personal_website/JScript/Jscript.js
+++ personal_website/JScript/Jscript.js
@@ -23,3 +23,21 @@ while (course.length != 4) {
     alert("Enter a Four Digit Course Number");
     var course = prompt("Check for Course", "Enter a four digit course number.")
 };
+
+function compareNums() {
+    for (var x of courseList) {
+        for (var y in x) {
+            if (x[y].includes(course)) {
+                console.log(`Yes I am taking the course ${x[y]} - ${x.name}`)
+                return true
+            }
+          
+        }
+    }
+  }
+  
+  if (compareNums() != true) {
+    var newClass = {code: course, name: null}
+    courseList.push(newClass)
+    console.log("New Class Added to Class List")
+  }
